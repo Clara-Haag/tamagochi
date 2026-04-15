3616270f9c4379697b726507f3d7fbdf68ff68a0
Clara Haag <75129653+Clara-Haag@users.noreply.github.com>
2026-04-14 21:29:10 -0300

    UPDATE: added styles to frontend + changed default name

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Tamagochi from "./js/tamagochi/tamagochi.js";
 import UI from "./js/ui/ui.js";
 import sleep from "./js/sleep.js";
 
-const tamagochi = new Tamagochi("Mimi");
+const tamagochi = new Tamagochi("Slimey");
 const ui = new UI();
 
 ui.bindEvents(tamagochi);
